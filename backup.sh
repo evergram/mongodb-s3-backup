b6f38af3e1c44e5ed6e84498fab20d430e0eaa4f
crimsonronin <joshstuartx@gmail.com>
2016-01-24 15:41:43 +1100

    Adds in directory support. Adds more verbosity to help debugging. Catches errors when trying to connect to get the secondary replica sets so it fails before trying to mongo dump.

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Argument = -i host -k key -s secret -b bucket -r region
+# Argument = -h host -k key -s secret -b bucket -r region
 #
 # To Do - Add logging of output.
 
@@ -14,12 +14,13 @@ usage: $0 options
 This script dumps the current mongo database, tars it, then sends it to an Amazon S3 bucket.
 
 OPTIONS:
-   -h      Show this message
-   -i      Mongo host
+   -u      Show this message
+   -h      Mongo host
    -k      AWS Access Key
    -s      AWS Secret Key
    -r      Amazon S3 region
    -b      Amazon S3 bucket name
+   -d      Amazon S3 directory (optional)
 EOF
 }
 
@@ -27,12 +28,13 @@ AWS_ACCESS_KEY=
 AWS_SECRET_KEY=
 S3_REGION=
 S3_BUCKET=
+S3_DIRECTORY=
 MONGODB_HOST=
 
-while getopts "ht:k:s:r:b:i:" OPTION
+while getopts "ud:h:k:s:r:b:" OPTION
 do
   case $OPTION in
-    h)
+    u)
       usage
       exit 1
       ;;
@@ -48,7 +50,10 @@ do
     b)
       S3_BUCKET=$OPTARG
       ;;
-    i)
+    d)
+      S3_DIRECTORY=$OPTARG
+      ;;
+    h)
       MONGODB_HOST=$OPTARG
       ;;
     ?)
@@ -66,31 +71,45 @@ fi
 
 # Get the directory the script is being run from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DIR
 
-# Store the current date in YYYY-mm-DD-HHMMSS
-DATE=$(date -u "+%F-%H%M%S")
+# Store the current date in YYYY-mm-DD-HHMM
+DATE=$(date -u "+%F-%H%M")
 FILE_NAME="backup-$DATE"
 ARCHIVE_NAME="$FILE_NAME.tar.gz"
 
+if [[ -z $S3_DIRECTORY ]]; then
+    S3_DIRECTORY="/"
+else
+    S3_DIRECTORY="/${S3_DIRECTORY}/"
+fi
+
+S3_LOCATION="https://${S3_BUCKET}.s3-${S3_REGION}.amazonaws.com${S3_DIRECTORY}${ARCHIVE_NAME}"
+
+echo -e "Backing up from ${MONGODB_HOST} to a local file ${ARCHIVE_NAME}.\n"
+
+echo -e "Checking mongo for replica set\n"
 # Get if server is part of replica, if yes get slave address
 MONGODB_INSTANCE=$(mongo --host $MONGODB_HOST admin --quiet --eval "var repl = rs.status(); if (repl.ok) { for (var i in repl.members) { if (repl.members[i].stateStr == 'SECONDARY') { printjson(repl.members[i].name); break; }}}" | sed s/\"//g)
 
-# If not replica, use localhost
-if [[ -z $MONGODB_INSTANCE ]]
-then
-  MONGODB_INSTANCE="localhost:27017"
+# If no replica, use localhost
+if [[ -z $MONGODB_INSTANCE ]]; then
+    echo -e "No secondary instance found. Using localhost.\n"
+    MONGODB_INSTANCE="localhost:27017"
+elif [[ $MONGODB_INSTANCE =~ .*Error.* ]]; then
+    echo -e "Error connecting to mongo:\n\n ${MONGODB_INSTANCE}\n"
+    exit 1
+else
+    echo -e "Found secondary instance: ${MONGODB_INSTANCE}.\n"
 fi
 
-# Lock the database
-# Note there is a bug in mongo 2.2.0 where you must touch all the databases before you run mongodump
-mongo --host $MONGODB_INSTANCE admin --eval "var databaseNames = db.getMongo().getDBNames(); rs.slaveOk(); for (var i in databaseNames) { printjson(db.getSiblingDB(databaseNames[i]).getCollectionNames()) }; printjson(db.fsyncLock());"
+echo -e "Running mongodump...\n"
 
 # Dump the database
 mongodump --host $MONGODB_INSTANCE --out $DIR/backup/$FILE_NAME
 
-# Unlock the database
-mongo --host $MONGODB_INSTANCE admin --eval "printjson(db.fsyncUnlock());"
+
+echo -e "Completed mongodump to ${DIR}/backup/${FILE_NAME}\n"
+echo -e "Compressing the backup\n"
 
 # Tar Gzip the file
 tar -C $DIR/backup/ -zcvf $DIR/backup/$ARCHIVE_NAME $FILE_NAME/
@@ -101,16 +120,22 @@ rm -r $DIR/backup/$FILE_NAME
 # Send the file to the backup drive or S3
 
 HEADER_DATE=$(date -u "+%a, %d %b %Y %T %z")
-CONTENT_MD5=$(openssl dgst -md5 -binary $DIR/backup/$ARCHIVE_NAME | openssl enc -base64)
+CONTENT_MD5=$(openssl dgst -md5 -binary ${DIR}/backup/${ARCHIVE_NAME} | openssl enc -base64)
 CONTENT_TYPE="application/x-download"
-STRING_TO_SIGN="PUT\n$CONTENT_MD5\n$CONTENT_TYPE\n$HEADER_DATE\n/$S3_BUCKET/$ARCHIVE_NAME"
-SIGNATURE=$(echo -e -n $STRING_TO_SIGN | openssl dgst -sha1 -binary -hmac $AWS_SECRET_KEY | openssl enc -base64)
+STRING_TO_SIGN="PUT\n${CONTENT_MD5}\n${CONTENT_TYPE}\n${HEADER_DATE}\n/${S3_BUCKET}${S3_DIRECTORY}${ARCHIVE_NAME}"
+SIGNATURE=$(echo -e -n ${STRING_TO_SIGN} | openssl dgst -sha1 -binary -hmac ${AWS_SECRET_KEY} | openssl enc -base64)
+
+echo -e "Uploading the backup to ${S3_LOCATION}\n"
 
 curl -X PUT \
---header "Host: $S3_BUCKET.s3-$S3_REGION.amazonaws.com" \
---header "Date: $HEADER_DATE" \
---header "content-type: $CONTENT_TYPE" \
---header "Content-MD5: $CONTENT_MD5" \
---header "Authorization: AWS $AWS_ACCESS_KEY:$SIGNATURE" \
---upload-file $DIR/backup/$ARCHIVE_NAME \
-https://$S3_BUCKET.s3-$S3_REGION.amazonaws.com/$ARCHIVE_NAME
+--header "Host: ${S3_BUCKET}.s3-${S3_REGION}.amazonaws.com" \
+--header "Date: ${HEADER_DATE}" \
+--header "content-type: ${CONTENT_TYPE}" \
+--header "Content-MD5: ${CONTENT_MD5}" \
+--header "Authorization: AWS ${AWS_ACCESS_KEY}:${SIGNATURE}" \
+--upload-file ${DIR}/backup/${ARCHIVE_NAME} \
+${S3_LOCATION}
+
+rm -r $DIR/backup/$ARCHIVE_NAME
+
+echo -e "Successfully completed the backup to ${S3_LOCATION}\n"
